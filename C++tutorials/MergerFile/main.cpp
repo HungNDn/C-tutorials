65b17f938de97da77cfaef7ecac09c3919fbc082
Hung <nghiemhungts6896@gmail.com>
2021-11-29 13:27:30 +0700

    Hung sua lai source code

--- C++tutorials/MergerFile/main.cpp
+++ C++tutorials/MergerFile/main.cpp
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <iostream>
+#include <vector>
 #include <iostream>
 #include <unistd.h>
 #include <thread>
@@ -11,7 +16,6 @@
 #include <stdint.h>
 #include <pthread.h>
 #include <string>
-#include <string.h>
 #include <signal.h>
 #include <sys/stat.h> 
 #include <sys/types.h> 
@@ -19,73 +23,95 @@
 
 using namespace std;
 
-void getFiles(std::string path, std::vector<std::string>& files)
+void listFilesRecursively(char *basePath, std::vector <std::string>& directories)
 {
-    std::vector<std::string> ownname;
-    char path0[1000];
-    char path1[1000];
-    char path2[2000];
+    char path[1000];
     struct dirent *dp;
-    struct dirent *dp1;
-    DIR *dir = opendir(path.c_str());
+    DIR *dir = opendir(basePath);
+
     if (!dir)
         return;
 
     while ((dp = readdir(dir)) != NULL)
     {
+        // printf("%s\n", dp->d_name);
         if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
         {
-            strcpy(path0, path.c_str());
-            strcat(path0, "/");
-            strcat(path0, dp->d_name);
-            DIR *dir1 = opendir(path0);
-            if (!dir1)
-            return;
-            while ((dp1 = readdir(dir1)) != NULL)
+            strcpy(path, basePath);
+            strcat(path, "/");
+            strcat(path, dp->d_name);
+            DIR *dir1 = opendir(path);
+            if(!dir1)
             {
-                if (strcmp(dp1->d_name, ".") != 0 && strcmp(dp1->d_name, "..") != 0)
-                {
-                    sprintf(path2,"%s/%s",path0, dp1->d_name); 
-                    files.push_back(std::string(path2));
-                    ownname.push_back(std::string(dp->d_name));
-                    strcpy(path1, path0);
-                    strcat(path1, "/");
-                    strcat(path1, dp1->d_name);
-                }
+                directories.push_back(path);
+            }
+            else
+            {
+                listFilesRecursively(path, directories);
             }
             closedir(dir1);
         }
     }
     closedir(dir);
 }
+
+int strContains(char* string, char* toFind)
+{
+    int slen = strlen(string);
+    int tFlen = strlen(toFind);
+    int found = 0;
+
+    if( slen >= tFlen )
+    {
+        for(int s=0, t=0; s<slen; s++)
+        {
+            do{
+
+                if( string[s] == toFind[t] )
+                {
+                    if( ++found == tFlen ) return 1;
+                    s++;
+                    t++;
+                }
+                else { s -= found; found=0; t=0; }
+
+            }while(found);
+        }
+        return 0;
+    }
+    else return -1;
+}
+
+
 int main()
 {
-    string a;
-    std::vector<std::string> files;
-    std::fstream file;
-    std::fstream file1;
+    std::fstream fileIn;
+    std::fstream fileOut;
 
-    std::string output = "/home/hungndn/C++tutorials/hung.txt";
+    char path[100];
+    std::vector <std::string> directories;
     
-    std::cout << "Nhap vao duong dan can gop file: " <<std::endl;
-    cin >> a;
-    getFiles(a, files);
-    std::cout << "file size :" << files.size() <<std::endl;
+    printf("Enter path to list files: ");
+    scanf("%s", path);
+    listFilesRecursively(path, directories);
 
-    file1.open(output, std::ios::out | std::ios::app);
-
-    for (int i = 0; i < files.size(); i++)
+    fileOut.open("/home/hungndn/Videos/C++github/C++tutorials/MergerFile/hung.txt", std::ios::out | std::ios::app);
+    for (int i = 0; i < directories.size(); i++)
     {
-        std::cout<< "path: " << files[i] << std::endl;
-        file.open(files[i], std::ios::in);
-        while (file) 
+        if(strContains((char*)directories[i].c_str(),(char*)".cpp") == 1 || strContains((char*)directories[i].c_str(),(char*)".hpp") == 1)
         {
-            std::string line;
-            std::getline(file, line);
-            file1 << line << endl;
+            std::cout << "Path :" <<  directories[i] << std::endl;
+            fileIn.open(directories[i], std::ios::in);
+            while (fileIn) 
+            {
+               std::string line;
+               std::getline(fileIn, line);
+               fileOut << line << endl;
+               std::cout << "line :" << line << std::endl;
+            }
+            fileIn.close();
         }
-        file.close();
     }
-    file1.close();
-    return 0; 
+    fileOut.close();
+    return 0;
 }
